3b962c55aab93d7ddf9e6eedcf8bb5cc5d07ed65
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-03-18 14:23:15 -0400

    base

--- index.php
+++ index.php
@@ -17,39 +17,4 @@
   <title>The Hello World program,in PHP</title>
 </head>
 
-<body>
-  <script defer src="https://code.getmdl.io/1.3.0/material.min.js">
-    function button1() {
-      alert("Hello, Wordl!")
-    }
-  </script>
-  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
-    <header class="mdl-layout__header">
-      <div class="mdl-layout__header-row">
-        <span class="mdl-layout-title">The Hello World program, in PHP</span>
-      </div>
-    </header>
-    <main class="mdl-layout__content">
-      <div class="right-image">
-        <img src="./images/red panda-gif.gif" alt="red panda gif" width="250" />
-      </div>
-      <div class="page-content-php">
-        <form action="answer.php" method="POST">
-          <p>Length calculation</p>
-          <button class="mdl-button mdl-js-button mdl-button--raised mdl-button--colored" onclick="button1()">
-            Hello, World!
-          </button>
-          <div class="mdl-textfield mdl-js-textfield">
-            <input class="mdl-textfield__input" type=" text" pattern="-?[0-9]*(\.[0-9]+)?" name="length">
-            <label class="mdl-textfield__label" for="length-input">Type length here ...</label>
-            <span class="mdl-textfield__error">Input is not a number!</span>
-          </div>
-          <br />
-          <!-- button -->
-        </form>
-      </div>
-    </main>
-  </div>
-</body>
-
 </html>
